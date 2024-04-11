48a0b1c6c65c5ce393e65d7692865f852cd780c4
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2024-04-10 19:50:51 -0600

    Update script.m

--- laboratorio/lab6/script.m
+++ laboratorio/lab6/script.m
@@ -3,7 +3,12 @@ Fs=1000;
 No = 100;
 n = 0:(No-1);       % Vector de tiempo
 x = cos(pi/50 * n); % Señal discreta
-stem(n,x)
+
+figure;
+stem(n,x);
+title('x(n)');
+xlabel('Time');
+ylabel('Amplitud');
 
 % DFT
 Xk = fft(x,No);
@@ -14,10 +19,18 @@ F= (-No/2 : (No/2 - 1))*dF; %  -Fs/2 : dF : Fs/2
 
 % Magnitud
 Xkmag = abs(Xk);
+figure;
 stem(F,fftshift(Xkmag));
+title('|X(k)|');
+xlabel('Frequency');
+ylabel('Magnitude');
 
 % Phase
 Im = imag(Xk);
 Im(abs(Im)<1e-3) = 0;
 Xphase = atan(Im./real(Xk));
+figure;
 stem(F,fftshift(Xphase));
+title('Phi(X(k))');
+xlabel('Frequency');
+ylabel('Angle');
